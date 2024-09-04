98715c0ea2bd6b403d8629fbedfdaddc3ba64987
Miguel Laurente <miguel.laurente@alignet.com>
2024-09-04 14:06:15 -0500

    base url para deploy

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: "https://migueldev31.github.io/encuentra_tu_mesa/"
+
+})
